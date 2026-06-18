cbff36f33a4a18c2aeec87156deac85b64c84fa6
Christof Kaufmann <christof.kaufmann@hs-bochum.de>
2026-06-18 08:41:50 +0200

    Change icon slightly (colors and rotation)

--- icon/plot_icon.py
+++ icon/plot_icon.py
@@ -3,11 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # %% logo
-
-rng = np.random.default_rng(0)
+# in light theme the yellow markers are barely visible and it looks like a funnel (filter glyph)
+# in dark the violet center is barely visible and the shape seems circular. Looks nice.
+rng = np.random.default_rng(1)
 
 # circular markers
-angles = np.arange(0, 360, 60)
+angles = np.arange(0, 360, 60) - 30 - 180
 x = np.cos(angles * np.pi / 180)
 y = np.sin(angles * np.pi / 180)
 
@@ -15,16 +16,17 @@ y = np.sin(angles * np.pi / 180)
 x = np.append(x, 0)
 y = np.append(y, 0)
 
-# colors (center yellow)
+# colors (center violet)
 c = rng.random(x.shape)
-c[-1] = 1
+c[-1] = -0.5
 
 # plot and export
 fig, ax = plt.subplots(figsize=(2, 2))
-plt.scatter(x, y, s=1150, c=c, marker='h')
-plt.axis('equal')
+plt.scatter(x, y, s=1400, c=c, marker='H')
+# plt.axis('')
 plt.axis('off')
 plt.xlim(-1.4, 1.4)
+plt.ylim(-1.4, 1.4)
 # fig.savefig('icon.svg', transparent=True, bbox_inches='tight')  # SVG not allowed in Marketplace
 fig.savefig('icon.png', transparent=True, bbox_inches='tight')
 
